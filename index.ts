1b7f09bd52f5dcddd08e885ef17d5fdfbcbeb7ff
Vladimir <frolad@gmail.com>
2021-04-23 19:01:50 -0700

    fix

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { P2P } from './src/P2P';
 export { StreamType } from './src/model';
 export type { User, SelectedDevices, WarningMessage } from './src/model';
-export { transmitMessage, SendMessage } from './src/Messages';
+export { transmitMessage, SendMessage, bindOnMessages, unbindOnMessages } from './src/Messages';
 export type { Messages } from './src/Messages';
