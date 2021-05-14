ee53ca61059d018c07f19366df244a497acb0c78
Vladimir <frolad@gmail.com>
2021-05-14 16:41:17 -0700

    global var

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-declare var __webrtc_logs:Map<string, string[]>
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,16 @@
+declare var __webrtc_logs:Map<string, string[]>
+
 export function logout(user_guid: string, print:boolean, ...args:any[]) {
     if (print) console.log(...args)
 
-    if (!globalThis.__webrtc_logs) {
-        globalThis.__webrtc_logs = new Map();
+    if (!__webrtc_logs) {
+        __webrtc_logs = new Map();
     }
 
-    globalThis.__webrtc_logs.set(
+    __webrtc_logs.set(
         user_guid,
         [
-            ...(globalThis.__webrtc_logs.get(user_guid) || []),
+            ...(__webrtc_logs.get(user_guid) || []),
             `${args.map(a => typeof a === "string" ? a : JSON.stringify(a)).join("")}`
         ]
     )
